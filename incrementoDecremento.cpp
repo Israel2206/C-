e17d7382c1e773984ea7fed58990a468a5f4dab5
Israel <brenoisrael931@email.com>
2025-07-14 10:03:56 -0300

    incremento e decremento

---
+++ incrementoDecremento.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n1,n2;
+
+    n1 = 0;
+    n2 = 10;
+
+    cout << n1 << endl;
+    n1 +=1;
+
+    n2--;
+
+    cout << n1 << endl;
+
+    cout << n2 << endl;
+
+    return 0;
+}
