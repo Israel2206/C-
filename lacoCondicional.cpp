c8d2830b97b3326d9c118c447932c06e9d5feb92
Israel <brenoisrael931@email.com>
2025-07-14 10:22:00 -0300

    Comando de decisão IF ELSE

--- lacoCondicional.cpp
+++ lacoCondicional.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
+using namespace std;
 
+int main(){
+
+    int num = 10;
+    char opc = 's';
+
+    if (num > 5){
+        cout << "O valor é maior que 5";
+    }else{
+        cout << opc << endl;
+    }
+    
+
+    return 0;
+}
