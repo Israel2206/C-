1f648c230b408aced6c6fff8a8a0300d4b5afbb9
Israel <brenoisrael931@email.com>
2025-07-14 09:35:09 -0300

    Declarações múltiplas de variáveis, Constantes

---
+++ multiplasVariaveis.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#define pi 3.1415
+#define canal cout << "Testando o define" << endl;
+int main(){
+    
+    int vidas = 3, tiros =500, life=100;
+
+    cout << pi << endl;
+    canal
+
+    return 0;
+}
